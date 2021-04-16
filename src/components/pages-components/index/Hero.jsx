0cd872ca62f781728e6ef649d7d1655e31a6c946
lulu70 <liorco70@yahoo.com>
2021-04-16 12:25:17 +0300

    added animation to hero

--- src/components/pages-components/index/Hero.jsx
+++ src/components/pages-components/index/Hero.jsx
@@ -1,16 +1,36 @@
 import React from "react"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import SizeWrapper from "../../reusables/SizeWrapper"
+import { gsap, Linear } from "gsap"
 
 const Hero = ({ data }) => {
+  const imageContainerRef = React.useRef()
+  function spin() {
+    gsap
+      .timeline({
+        defaults: {
+          duration: 0.2,
+          ease: Linear.easeInOut,
+        },
+      })
+      .to(imageContainerRef.current, {
+        rotate: 360,
+      })
+      .to(imageContainerRef.current, {
+        rotate: 0,
+      })
+  }
   return (
     <section id="hero" className="relative">
       <SizeWrapper className="my-96">
-        <GatsbyImage
-          image={getImage(data.yellowDuck)}
-          className="mx-auto block w-36"
-          alt="yellow-duck"
-        />
+        <div ref={imageContainerRef}>
+          <GatsbyImage
+            image={getImage(data.yellowDuck)}
+            className="mx-auto block w-36"
+            alt="yellow-duck"
+            onPointerEnter={spin}
+          />
+        </div>
         <h1 className="text-7xl text-right font-bold mt-12 block">
           LiCHTPiRATEN
         </h1>
